4c616c3eaa9e610ea958ca1107cbb6339a7d776d
bboden5000 <97046809+bboden5000@users.noreply.github.com>
2022-04-19 10:18:06 -0600

    Update BenjaminBoden_M11.py
    Added code

---
+++ BenjaminBoden_M11.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Name: Benjamin M. Boden
+# Class: (INFO 1200)
+# Section: (X01)
+# Professor: (Crandall)
+# Date: 18/03/12022
+# Project #: MO7
+''' I declare that the source code contained in this assignment was written solely by me.
+    I understand that copying any source code, in whole or in part, 
+    constitutes cheating, and that I will receive a zero on this project
+    if I am found in violation of this policy.'''
+
+
+# Name: main () function
+# Description: main program function
+# Peramiters: none
+def main ():
+    pass
+
+# Run main () program function
+if __name__ == "__main__" : main ()
