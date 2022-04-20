59a42706f16b28793afeaef39fe0b06839fbdc50
bboden5000 <benjamin@2canex.com>
2022-04-20 11:22:10 -0600

    Update BenjaminBoden_M11.py
    Reusing old code, and making new

--- BenjaminBoden_M11.py
+++ BenjaminBoden_M11.py
@@ -4,24 +4,24 @@
 # Class: (INFO 1200)
 # Section: (X01)
 # Professor: (Crandall)
-# Date: 16/04/12022
-# Project #: M10
+# Date: 20/04/12022
+# Project #: M11
 ''' I declare that the source code contained in this assignment was written solely by me.
     I understand that copying any source code, in whole or in part, 
     constitutes cheating, and that I will receive a zero on this project
     if I am found in violation of this policy.'''
 
 
-import Monthly_Sales_class as MS # import my monthly sales modul
+import Monthly_Sales_class as MS # import my monthly comtacts module
 
-FILENAME = "MO10_Project\Python-csv-Reader\monthly_sales.csv" # declar the file path constant
+FILENAME = "MO11_Project\contacts.csv" # declare the file path constant
 
 
 # Function: display_title()
 # Description: print the program tital to consol
 # Perameters: None
 def display_title():
-    print("Benjamin Boden's Monthly Sales") # Tital
+    print("Benjamin Boden's Contact Database") # Tital
     print() # print new line
 
 
@@ -31,10 +31,11 @@ def display_title():
 def display_menu():
     print('''COMMAND MENU
 
-            monthly - View monthly sales
-            yearly  - View yearly summary
-            edit    - Edit sales for a month
-            exit    - Exit program''') # print the menu witht this formating
+            list - Display all contacts
+            view - View a contact
+            add - Add a contact
+            del - Delete a contact
+            exit - Exit program''') # print the menu witht this formating
     print() # new line
 
 
@@ -42,23 +43,25 @@ def display_menu():
 # Description: main program function
 # Peramiters: None
 def main ():
-    sales = [] # declare variable
+    comtacts = [] # declare variable
 
-    monthly_sales = MS.Monthly_Sales(FILENAME) # creat new object
+    contact_list = MS.Monthly_Sales(FILENAME) # creat new object
 
     display_title() # print tital
     display_menu() # print menu
     
-    sales = monthly_sales.read_file() # get the csv contents
+    comtacts = contact_list.read_file() # get the csv contents
 
     while True: # run forever
         command = input("Command: ") # get user input
-        if command == "monthly":
-            monthly_sales.view_monthly_sales(sales) # if this comand do that
-        elif command == "yearly":
-            monthly_sales.view_yearly_summary(sales)  # if this comand do that
-        elif command == "edit":
-            monthly_sales.edit(sales) # if this comand do that
+        if command == "lsit":
+         contact_list.view_monthly_sales(comtacts) # if this comand do that
+        elif command == "view":
+         contact_list.view_yearly_summary(comtacts)  # if this comand do that
+        elif command == "add":
+         contact_list.edit(comtacts) # if this comand do that
+        elif command == "del":
+            pass
         elif command == "exit":
             break # if this comand do that
         else:
