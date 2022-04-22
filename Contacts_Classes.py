6c15d93f29cd8cc376b7beed4d99e15ef70a21b2
bboden5000 <benjamin@2canex.com>
2022-04-22 10:25:29 -0600

    Added documentation

--- Contacts_Classes.py
+++ Contacts_Classes.py
@@ -4,33 +4,35 @@
 # Class: (INFO 1200)
 # Section: (X01)
 # Professor: (Crandall)
-# Date: 16/04/12022
-# Project #: M10
+# Date: 22/04/12022
+# Project #: M11
 ''' I declare that the source code contained in this assignment was written solely by me.
     I understand that copying any source code, in whole or in part, 
     constitutes cheating, and that I will receive a zero on this project
     if I am found in violation of this policy.'''
 
 
-import csv # get csv tools
-import sys
-import Contact_Obj as CO
+
+import sys # include dependencies
+import Contact_Obj as CO # include dependencies
 
 # Class: Contact_Consol
 # Purpos: gets and munipulates all the csv data for user
 # Perameters: path => the location of the text file
 class Contact_Consol():
     PATHNAME:str # text file location
-    contacts:object
+    contacts:object # file list object
 
     def __init__(self, path):
         self.PATHNAME = path # sets the path name for the object
-        self.consol_loop()
+        self.consol_loop() # start running main loop
         
 
-    
+    # function: consol_loop()
+    # Job: program loop
+    # perameters: self => self
     def consol_loop (self):
-        self.contacts = CO.Contacts_Obj(self.PATHNAME)
+        self.contacts = CO.Contacts_Obj(self.PATHNAME) # initiat object
         
         ###contacts = self.read_file() # get the csv contents
 
@@ -87,58 +89,68 @@ class Contact_Consol():
         print() # print a blank line
 
 
-    # Function: view_yearly_summary()
-    # Purpos: calculates a data sumery to print to screen
-    # Peramaters: contacts => the local linked list to be summariesd
+    # Function: view ()
+    # Purpos: shows a single contact to be used
+    # Peramaters: contacts_obj => the object containing the liked list
     def view (self, contact_obj):
-        contacts = contact_obj.get_CONTACTS()
-        number = self.get_contact_number(contacts)
+        contacts = contact_obj.get_CONTACTS() # get the objects list
+        number = self.get_contact_number(contacts) # get user input
 
-        if number >= 1:
-            contact = contacts[number-1]
-            print("Name:", contact[0])
-            print("Email:", contact[1])
-            print("Phone:", contact[2])
-            print() 
-        else:
-            pass
+        if number >= 1: # if the number is in range do:
+            contact = contacts[number-1] # get the list line from the 2D list
+            print("Name:", contact[0]) # print the list
+            print("Email:", contact[1]) # print the list
+            print("Phone:", contact[2]) # print the list
+            print() # print new line
+        else: # or do this:
+            pass # do nothing
 
-
+    
+    # Function: get_contact_number()
+    # Job: Gets a valed number form the user
+    # Perameters: self => self, contacts => 2D linked list to be used to validate agians
     def get_contact_number(self, contacts):
-        while True:
-            try:
-                number = int(input("Number: "))
-            except ValueError:
-                print("Invalid integer.\n")
-                return -1
-                
-            if number < 1 or number > len(contacts):
-                print("Invalid contact number.\n")
-                return -1
-            else:
-                return number
+        while True: # start infinet loop
+            try: # first try this
+                number = int(input("Number: ")) # get user input
+            except ValueError: # if this error acurs
+                print("Invalid integer.\n") # print message
+                continue # restart the loop
+            
+            if number < 1 or number > len(contacts): # since no errors acurd, check if in range of list
+                print("Invalid contact number.\n") # tell user its wrong
+                continue # restart loop
+            else: # or its valed:
+                return number # return the valed number
 
     
-    def add(self, contact_obj):
-        name = input("Name: ")
-        email = input("Email: ")
-        phone = input("Phone: ")
+    # function: add()
+    # Job: adds and entry to the 2D list and saves to CSV file
+    # Peramiters: self => self, contact_obj => the object conating the 2D list
+    def add(self, contact_obj): 
+        name = input("Name: ") # get user input as string
+        email = input("Email: ") # get user input as string
+        phone = input("Phone: ") # get user input as string
 
-        contact = []
-        contact.append(name)
-        contact.append(email)
-        contact.append(phone)
-        
-
-        print (f"{contact[0]} was added.")
-        print ()
-        contact_obj.set_CONTACTS(contact)
+        contact = [] # declare short list
+        contact.append(name) # add varaible to list
+        contact.append(email) # add variable to list
+        contact.append(phone) # add variable to list
+         
+        print (f"{contact[0]} was added.") # notify user
+        print () # print new lin
+        contact_obj.set_CONTACTS(contact) # save to master object
 
     
+    # Function: delete()
+    # Job: removes a contact from the 2D list
+    # peramaters: self => self, contact_obj => the object containg the 2D list
     def delete(self, contact_obj):
-        contacts = contact_obj.get_CONTACTS()
-        num = self.get_contact_number(contacts) - 1
-        print(f"{contacts[num]} was deleted.\n")
-        contact_obj.remove_CONTACTS(num)
+        contacts = contact_obj.get_CONTACTS() # gets the 2D list from the object
+        
+        num = self.get_contact_number(contacts) - 1 # gets valed user input
+        print(f"{contacts[num]} was deleted.\n") # print message to user
+        
+        contact_obj.remove_CONTACTS(num) # removes the contact from the master object
         
         
